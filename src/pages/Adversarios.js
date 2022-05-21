6fba67c0ab00e1c3572972f3c57f3e9e9e2612fe
Henrique Filho <hbaf71@gmail.com>
2022-05-21 15:04:38 -0300

    iterando times na página adversários

--- src/pages/Adversarios.js
+++ src/pages/Adversarios.js
@@ -2,9 +2,23 @@ import React from 'react';
 import Times from '../Times';
 
 function Adversarios(props) {
+  var adversarios = ['Palmeiras', 'Corinthians'];
+
   return (
-    <div className="App-header" style={{backgroundColor: Times(props.meuTime).backgroundColor, color: Times(props.meuTime).letterColor}}>
-        <h1>Adversários</h1>
+    <div className="App-header" style={{ backgroundColor: Times(props.meuTime).backgroundColor, color: Times(props.meuTime).letterColor }}>
+      <h1>Adversários</h1>
+      <table style={{ border: Times(props.meuTime).letterColor, borderStyle: 'solid' }}>
+        <tbody>
+          {
+            adversarios.map(function (i) {
+              return <tr>
+                <td><img src={require('../escudos/'+Times(Times(i).nomeAtual).escudo+'.png')} alt='escudo' height='100' width='100' /></td>
+                <td>{Times(i).nomeAtual}</td>
+              </tr>
+            })
+          }
+        </tbody>
+      </table>
     </div>
   )
 }
--- src/pages/Inicio.js
+++ src/pages/Inicio.js
@@ -3,13 +3,11 @@ import Times from '../Times';
 
 function Inicio(props) {
   return (
-    <div className="App">
-    <header className="App-header" style={{backgroundColor: Times(props.meuTime).backgroundColor}}>
+    <div className="App-header" style={{backgroundColor: Times(props.meuTime).backgroundColor}}>
       <img src={require('../escudos/'+Times(props.meuTime).escudo+'.png')} className="App-logo" alt={"Escudo do "+props.meuTime} />
       <p style={{color: Times(props.meuTime).letterColor}}>
         Eu sou {Times(props.meuTime).nomeAtual}
       </p>
-    </header>
   </div>
   )
 }
