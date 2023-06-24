424441433741dcfaa87ef29d1d749ebdd087458c
Henrique Filho <hbaf71@gmail.com>
2023-06-24 15:34:40 -0300

    pegando jogo do firebase

--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -1,14 +1,22 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import Times from '../Times';
 import Tabs from '../components/Tabs';
 import { AuthGoogleContext } from '../contexts/authGoogle';
 import Estatisticas from '../components/Estatisticas';
+import { app } from '../services/firebaseConfig';
+import { collection, getFirestore, getDocs } from "firebase/firestore";
 
 function Perfil(props) {
   const { user, signOut } = useContext(AuthGoogleContext);
   const meuTime = "Botafogo";
   let userLogado;
   let nomeUsuario;
+  let userFound;
+  let meusJogos = [];
+  let isLoading = true;
+  const db = getFirestore(app);
+  const usersCollectionRef = collection(db, "users");
+  let [users, setUsers] = useState([]);
 
   try {
     userLogado = JSON.parse(user);
@@ -16,16 +24,32 @@ function Perfil(props) {
     userLogado = user;
   }
 
+  useEffect(() => {
+    const getUsers = async () => {
+      let data = await getDocs(usersCollectionRef);
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    }
+    getUsers();
+  }, []);
+
+  const getJogos = async () => {
+    try {
+      userFound = users.find(userJogo =>
+        userJogo.UID === userLogado.uid
+      );
+      for (let a = 0; a < userFound.jogos.length; a++) {
+        meusJogos.push(JSON.parse(userFound.jogos[a]))
+      }
+      isLoading = false;
+    } catch (error) {}
+  }
+  getJogos();
+  
   try {
     nomeUsuario = userLogado.displayName.split(" ")[0] + " " + userLogado.displayName.split(" ")[1];
   } catch (error) {
     nomeUsuario = userLogado.displayName.split(" ")[0];
   }
-
-  let meusJogos = props.meusJogos;
-  meusJogos.sort(function (a, b) {
-    return a.data < b.data ? -1 : a.data > b.data ? 1 : 0;
-  });
 
   return (
     <>
@@ -40,7 +64,9 @@ function Perfil(props) {
           {meusJogos.length > 0 && <Estatisticas meuTime={meuTime} jogos={meusJogos} />}
         </div>
         <button onClick={() => signOut()}>Sair</button>
-        <Tabs meuTime={meuTime} meusJogos={meusJogos} />
+        {meusJogos.length > 0 && <Tabs meuTime={meuTime} meusJogos={meusJogos} />}
+        {isLoading && <p>Carregando jogos...</p>}
+        {!isLoading && meusJogos.length === 0 && <p>Nenhum jogo cadastrado</p>}
       </div>
     </>
   )
