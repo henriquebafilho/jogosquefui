d272c449eb29e434ad1697a6a845f107a91ec30a
Henrique Filho <hbaf71@gmail.com>
2023-07-08 12:46:32 -0300

    voltar ao topo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { AuthGoogleProvider } from './contexts/authGoogle';
 import { AppRoutes } from './routes/routes';
+import VoltarAoTopo from '../src/components/VoltarAoTopo';
 
 export function App() {
   return (
     <AuthGoogleProvider>
       <AppRoutes />
+      <VoltarAoTopo/>
     </AuthGoogleProvider>
   );
 }
