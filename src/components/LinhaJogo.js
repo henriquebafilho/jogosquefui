be440ab26f31a0f88db55f4f95c6a6ffc884bf46
Henrique Filho <hbaf71@gmail.com>
2025-03-12 02:56:02 -0300

    atualizando cor dos icones

--- src/components/LinhaJogo.js
+++ src/components/LinhaJogo.js
@@ -74,7 +74,7 @@ class LinhaJogo extends Component {
                 key={JSON.stringify(this.props.jogo)}
             >
                 <div>
-                    <FaCalendarAlt style={{ color: Times(this.props.jogo.mandante).backgroundColor === "white" ? "black" : "white", paddingRight: "5px" }} />
+                    <FaCalendarAlt style={{ color: Times(this.props.jogo.mandante).letterColor, paddingRight: "5px" }} />
                     <p style={{
                         textShadow: "0 0 3px #000000, 0 0 3px #000000, 0 0 3px #000000, 0 0 3px #000000, 0 0 3px #000000",
                         color: "white",
@@ -84,8 +84,8 @@ class LinhaJogo extends Component {
                     </p>
                 </div>
                 <div>
-                    <p style={{ textShadow: "0 0 3px #000000, 0 0 3px #000000, 0 0 3px #000000, 0 0 3px #000000, 0 0 3px #000000", color: "white", fontSize: '1em' }}><FaTrophy style={{ color: Times(this.props.jogo.mandante).backgroundColor === "white" ? "black" : "white" }} /> {this.props.jogo.campeonato}</p>
-                    <p style={{ textShadow: "0 0 3px #000000, 0 0 3px #000000, 0 0 3px #000000, 0 0 3px #000000, 0 0 3px #000000", color: "white", marginBottom: '5px', fontSize: '1em' }}><MdStadium style={{ color: Times(this.props.jogo.mandante).backgroundColor === "white" ? "black" : "white" }} /> {this.props.jogo.estadio !== "" ? this.props.jogo.estadio : " - "}</p>
+                    <p style={{ textShadow: "0 0 3px #000000, 0 0 3px #000000, 0 0 3px #000000, 0 0 3px #000000, 0 0 3px #000000", color: "white", fontSize: '1em' }}><FaTrophy style={{ color: Times(this.props.jogo.mandante).letterColor }} /> {this.props.jogo.campeonato}</p>
+                    <p style={{ textShadow: "0 0 3px #000000, 0 0 3px #000000, 0 0 3px #000000, 0 0 3px #000000, 0 0 3px #000000", color: "white", marginBottom: '5px', fontSize: '1em' }}><MdStadium style={{ color: Times(this.props.jogo.mandante).letterColor }} /> {this.props.jogo.estadio !== "" ? this.props.jogo.estadio : " - "}</p>
                 </div>
                 <div id='placar' style={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}>
                     {/* (this.props.jogo[2] !== "" && this.props.jogo[3] !== "") && <div style={{ display: 'inline', textAlign: 'left' }}>
