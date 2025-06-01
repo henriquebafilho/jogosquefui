b33701044f026d895c6235879621fb038920a650
Henrique Filho <hbaf71@gmail.com>
2025-06-01 18:01:47 -0300

    Atualizar o ProximosJogos.js

--- src/TodosOsJogos/ProximosJogos.js
+++ src/TodosOsJogos/ProximosJogos.js
@@ -32,7 +32,7 @@ function jogos() {
     jogos.push({ "mandante": "Botafogo", "visitante": "Seattle Sounders", "golsMandante": "", "golsVisitante": "", "campeonato": "Super Mundial de Clubes 2025", "data": "2025-06-15", "estadio": "Lumen Field", "horario": "23:00" });
     jogos.push({ "mandante": "Botafogo", "visitante": "Mirassol", "golsMandante": "", "golsVisitante": "", "campeonato": "Brasileirão 2025", "data": "2025-06-11", "estadio": "Nilton Santos" });
     jogos.push({ "mandante": "Botafogo", "visitante": "Ceará", "golsMandante": "", "golsVisitante": "", "campeonato": "Brasileirão 2025", "data": "2025-06-04", "estadio": "Nilton Santos", "horario": "20:00" });
-    jogos.push({ "mandante": "Santos", "visitante": "Botafogo", "golsMandante": "", "golsVisitante": "", "campeonato": "Brasileirão 2025", "data": "2025-06-01", "estadio": "Vila Belmiro", "horario": "16:00" });
+    jogos.push({ "mandante": "Santos", "visitante": "Botafogo", "golsMandante": 0, "golsVisitante": 1, "campeonato": "Brasileirão 2025", "data": "2025-06-01", "estadio": "Vila Belmiro", "horario": "16:00" });
     jogos.push({ "mandante": "Botafogo", "visitante": "Universidad de Chile", "golsMandante": 1, "golsVisitante": 0, "campeonato": "Libertadores 2025", "data": "2025-05-27", "estadio": "Nilton Santos", "horario": "21:30" });
 
     jogos.sort(function (a, b) {
