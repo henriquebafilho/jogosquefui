e66240369a64cb60962f0771c3cda3f279ec9ce1
Henrique Filho <hbaf71@gmail.com>
2025-07-02 23:27:23 -0300

    Tirando Estatísticas do Anos

--- src/pages/Anos.js
+++ src/pages/Anos.js
@@ -3,7 +3,6 @@ import Times from '../Times';
 import common from '../common';
 import BotafogoJogos from '../TodosOsJogos/BotafogoJogos';
 import { NavLink } from 'react-router-dom';
-import Estatisticas from '../components/Estatisticas';
 
 class Anos extends Component {
   constructor(props) {
@@ -77,7 +76,6 @@ class Anos extends Component {
         <div className="App-header" style={{ backgroundColor: Times(this.props.time).backgroundColor, color: Times(this.props.time).letterColor, alignItems: 'normal' }}>
           <table>
             <tbody>
-              <Estatisticas meuTime={time} jogos={this.state.jogos} />
               {this.state.isLoading && <h1>Carregando...</h1>}
               <input
                 type="number"
