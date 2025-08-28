f8d830bff10d070bf3e5ac303da6efd0e18d336d
Henrique Filho <hbaf71@gmail.com>
2025-08-27 23:27:23 -0300

    Atualizar o ProximosJogos.js

--- src/TodosOsJogos/ProximosJogos.js
+++ src/TodosOsJogos/ProximosJogos.js
@@ -21,7 +21,7 @@ function jogos() {
     jogos.push({ "mandante": "São Paulo", "visitante": "Botafogo", "golsMandante": "", "golsVisitante": "", "campeonato": "Brasileirão 2025", "data": "2025-09-14", "estadio": "Morumbi", "horario": "17:30" });
     jogos.push({ "mandante": "Botafogo", "visitante": "Vasco", "golsMandante": "", "golsVisitante": "", "campeonato": "Copa do Brasil 2025", "data": "2025-09-11", "estadio": "Nilton Santos", "horario": "21:30" });
     jogos.push({ "mandante": "Botafogo", "visitante": "Red Bull Bragantino", "golsMandante": "", "golsVisitante": "", "campeonato": "Brasileirão 2025", "data": "2025-08-30", "estadio": "Nilton Santos", "horario": "18:30" });
-    jogos.push({ "mandante": "Vasco", "visitante": "Botafogo", "golsMandante": "", "golsVisitante": "", "campeonato": "Copa do Brasil 2025", "data": "2025-08-27", "estadio": "São Januário", "horario": "21:30" });
+    jogos.push({ "mandante": "Vasco", "visitante": "Botafogo", "golsMandante": 1, "golsVisitante": 1, "campeonato": "Copa do Brasil 2025", "data": "2025-08-27", "estadio": "São Januário", "horario": "21:30" });
     
     jogos.sort(function (a, b) {
         return a.data < b.data ? -1 : a.data > b.data ? 1 : 0;
