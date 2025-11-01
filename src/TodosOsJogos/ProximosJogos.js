6349641dda4620c82516f4b5a746e625659b34e8
Henrique Filho <hbaf71@gmail.com>
2025-11-01 19:54:32 -0300

    Atualizar o ProximosJogos.js

--- src/TodosOsJogos/ProximosJogos.js
+++ src/TodosOsJogos/ProximosJogos.js
@@ -14,7 +14,7 @@ function jogos() {
     jogos.push({ "mandante": "Botafogo", "visitante": "Sport", "golsMandante": "", "golsVisitante": "", "campeonato": "Brasileirão 2025", "data": "2025-12-06", "estadio": "Nilton Santos" });
     jogos.push({ "mandante": "Vitória", "visitante": "Botafogo", "golsMandante": "", "golsVisitante": "", "campeonato": "Brasileirão 2025", "data": "2025-11-09", "estadio": "Barradão", "horario": "16:00" });
     jogos.push({ "mandante": "Botafogo", "visitante": "Vasco", "golsMandante": "", "golsVisitante": "", "campeonato": "Brasileirão 2025", "data": "2025-11-05", "estadio": "Nilton Santos", "horario": "19:30" });
-    jogos.push({ "mandante": "Mirassol", "visitante": "Botafogo", "golsMandante": "", "golsVisitante": "", "campeonato": "Brasileirão 2025", "data": "2025-11-01", "estadio": "José Maria de Campos Maia", "horario": "18:30" });
+    jogos.push({ "mandante": "Mirassol", "visitante": "Botafogo", "golsMandante": 0, "golsVisitante": 0, "campeonato": "Brasileirão 2025", "data": "2025-11-01", "estadio": "José Maria de Campos Maia", "horario": "18:00" });
     jogos.push({ "mandante": "Botafogo", "visitante": "Santos", "golsMandante": 2, "golsVisitante": 2, "campeonato": "Brasileirão 2025", "data": "2025-10-26", "estadio": "Nilton Santos", "horario": "16:00" });
     jogos.push({ "mandante": "Ceará", "visitante": "Botafogo", "golsMandante": 0, "golsVisitante": 2, "campeonato": "Brasileirão 2025", "data": "2025-10-19", "estadio": "Castelão-CE", "horario": "18:30" });
     jogos.push({ "mandante": "Botafogo", "visitante": "Flamengo", "golsMandante": 0, "golsVisitante": 3, "campeonato": "Brasileirão 2025", "data": "2025-10-15", "estadio": "Nilton Santos", "horario": "19:30" });
