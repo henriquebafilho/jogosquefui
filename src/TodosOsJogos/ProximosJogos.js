8c1ac96287d3df41f85636f72835ca658f57d261
Henrique Filho <hbaf71@gmail.com>
2026-02-04 23:34:12 -0300

    Atualizar o ProximosJogos.js

--- src/TodosOsJogos/ProximosJogos.js
+++ src/TodosOsJogos/ProximosJogos.js
@@ -40,7 +40,7 @@ function jogos() {
     jogos.push({ "mandante": "Nacional Potosí", "visitante": "Botafogo", "golsMandante": "", "golsVisitante": "", "campeonato": "Libertadores 2026", "data": "2026-02-18", "estadio": "Víctor Agustín Ugarte", "horario": "21:30" });
     jogos.push({ "mandante": "Fluminense", "visitante": "Botafogo", "golsMandante": "", "golsVisitante": "", "campeonato": "Brasileirão 2026", "data": "2026-02-12", "estadio": "Maracanã", "horario": "19:30" });
     jogos.push({ "mandante": "Vasco", "visitante": "Botafogo", "golsMandante": "", "golsVisitante": "", "campeonato": "Carioca 2026", "data": "2026-02-08", "estadio": "São Januário", "horario": "18:00" });
-    jogos.push({ "mandante": "Grêmio", "visitante": "Botafogo", "golsMandante": "", "golsVisitante": "", "campeonato": "Brasileirão 2026", "data": "2026-02-04", "estadio": "Arena do Grêmio", "horario": "21:30" });
+    jogos.push({ "mandante": "Grêmio", "visitante": "Botafogo", "golsMandante": 5, "golsVisitante": 3, "campeonato": "Brasileirão 2026", "data": "2026-02-04", "estadio": "Arena do Grêmio", "horario": "21:30" });
     jogos.push({ "mandante": "Botafogo", "visitante": "Fluminense", "golsMandante": 0, "golsVisitante": 1, "campeonato": "Carioca 2026", "data": "2026-02-01", "estadio": "Nilton Santos", "horario": "20:30" });
     jogos.push({ "mandante": "Botafogo", "visitante": "Cruzeiro", "golsMandante": 4, "golsVisitante": 0, "campeonato": "Brasileirão 2026", "data": "2026-01-29", "estadio": "Nilton Santos", "horario": "21:30" });
     jogos.push({ "mandante": "Botafogo", "visitante": "Bangu", "golsMandante": 2, "golsVisitante": 0, "campeonato": "Carioca 2026", "data": "2026-01-24", "estadio": "Nilton Santos", "horario": "21:00" });
